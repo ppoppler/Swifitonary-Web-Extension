8c07a8637bf1ebe3359d04841c8a09f7a6a4a481
Philip Poppler <philpoppler@gmail.com>
2019-02-11 16:53:06 -0800

    Add MongoDB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const unirest = require("unirest");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+/**
+ * Connecting to the server
+ */
+mongoose
+  .connect(
+    "mongodb://admin:swiftaf@swiftionary-shard-00-00-inusj.mongodb.net:27017,swiftionary-shard-00-01-inusj.mongodb.net:27017,swiftionary-shard-00-02-inusj.mongodb.net:27017/test?ssl=true&replicaSet=Swiftionary-shard-0&authSource=admin&retryWrites=true"
+  )
+  .then(
+    () => {
+      console.log("Connected to Swiftionary cluster on MongoDB Atlas");
+    },
+    err => {
+      console.log("Failed to connected to Swiftionary cluster");
+    }
+  );
 
+const PORT = process.env.PORT || 5000;
 
 /**
  * Define GET Request from WordsAPI
@@ -24,7 +40,6 @@ app.get("/define", function(req, res) {
     });
 });
 
-
 /**
  * Synonym GET Request from WordsAPI
  */
@@ -37,24 +52,42 @@ app.get("/synonym", function(req, res) {
       "0339b5007bmshc2b0c44dc045fdep1e9fd9jsn9c50eeaea8b8"
     )
     .end(function(result) {
-        console.log(result.body);
-        res.send(result.body);
+      console.log(result.body);
+      res.send(result.body);
     });
 });
 
 /**
  * Antonym GET Request from Words API
  */
-app.get("/antonym", function(req, res){
-    const word = req.query.word;
-    unirest
+app.get("/antonym", function(req, res) {
+  const word = req.query.word;
+  unirest
     .get(`https://wordsapiv1.p.mashape.com/words/${word}/antonyms`)
     .header(
       "X-Mashape-Key",
       "0339b5007bmshc2b0c44dc045fdep1e9fd9jsn9c50eeaea8b8"
-    ).end(function(result){
-        console.log(result.body);
-        res.send(result.body);
+    )
+    .end(function(result) {
+      console.log(result.body);
+      res.send(result.body);
+    });
+});
+
+/**
+ * Rhyme GET Request from WordsAPI
+ */
+app.get("/rhyme", function(req, res) {
+  const word = req.query.word;
+  unirest
+    .get(`https://wordsapiv1.p.mashape.com/words/${word}/rhymes`)
+    .header(
+      "X-Mashape-Key",
+      "0339b5007bmshc2b0c44dc045fdep1e9fd9jsn9c50eeaea8b8"
+    )
+    .end(function(result) {
+      console.log(result.body);
+      res.send(result.body);
     });
 });
 
