ec76478b5ae89ce4b3821b9394925daa57be4add
AlteredReality <38482035+AlteredReality@users.noreply.github.com>
2019-04-22 17:39:07 -0700

    Shows the spellcheck suggestion and corrections

--- react-frontend2/src/SpellCheck.js
+++ react-frontend2/src/SpellCheck.js
@@ -2,8 +2,7 @@
 import React, { Component } from "react";
 import { getspellCheck } from "./BackendHelpers";
 
-export default class spellCheck extends Component 
-{
+export default class spellCheck extends Component {
   state = {
     text: "",
     spellcheck: null
@@ -29,22 +28,43 @@ export default class spellCheck extends Component
   }
 
   render() {
+    var correctedWords = [];
+    if (this.state.spellcheck !== null) {
+      for (var key in this.state.spellcheck.corrections) {
+        if (this.state.spellcheck.corrections.hasOwnProperty(key)) {
+          correctedWords.push(key);
+          
+        }
+      }
+    } else {
+      console.log("null else has been hit ");
+    }
     return (
       <div className="container main-def">
         <h2 className="Montserrat">{this.state.text}</h2>
         {this.state.spellcheck !== null && (
           <div>
             <h3 className="Montserrat">spellCheck</h3>
-              <div>
-                <hr />
-                <p className="Montserrat">{JSON.stringify(this.state.spellcheck.suggestion)}</p>
-              </div>
-            {/* {this.state.spellcheck.corrections..map(info => (
-              <div>
-                <hr />
-                <p className="Montserrat">{JSON.stringify(info)}</p>
-              </div>
-            ))} */}
+            <div>
+              <hr />
+              {correctedWords.map(info => 
+                <div>
+                  <h4 className="Montserrat">{info}</h4>
+                  <hr/>
+                  <p>Corrections: </p>
+                {this.state.spellcheck.corrections[info].map(
+                  correctionOutput => 
+                    <div>
+                      {console.log(correctionOutput)}
+                      
+                      <p className="Montserrat">{String(correctionOutput)}</p>
+                    </div>
+                  
+                )}
+                </div>
+              )}
+              {/* <p className="Montserrat">{JSON.stringify(this.state.spellcheck.corrections)}</p> */}
+            </div>
           </div>
         )}
       </div>
